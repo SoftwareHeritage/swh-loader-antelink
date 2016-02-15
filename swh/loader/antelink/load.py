f2750fccd9f31922cdf2ee7814d69eed911ff974
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-02-15 14:35:44 +0100

    Load aws ls s3 data with size
    Simplify calling the data loading:
    
    ```sh
    cat ls-antelink-object-storage-files | python3 -u -m swh.loader.antelink.load "service=antelink-swh" | tee stats/round-9
    ```

--- swh/loader/antelink/load.py
+++ swh/loader/antelink/load.py
@@ -8,38 +8,30 @@ antelink.content_s3 table."""
 
 
 import os
-import glob
 import sys
 
 from swh.loader.antelink.db import Db
-
-
-DB_CONN = "service='antelink-swh'"
 
 
 def load_file(path):
     """Load file and yield sha1, pathname couple."""
     with open(path, 'r') as f:
         for line in f:
-            data = line.rstrip('\n').split(' ')
+            data = line.rstrip().split(' ')
             l = len(data)
             pathname = data[l - 1]
-            size = data[l - 2]
             if pathname.endswith('.gz'):
                 sha1 = bytes.fromhex(os.path.basename(pathname).split('.')[0])
+                size = data[l - 2]
                 yield {'sha1': sha1,
                        'path': pathname,
                        'size': size}
 
 
 def store_file_to_antelink_db(db, path):
-    try:
-        with db.transaction() as cur:
-            db.copy_to(list(load_file(path)), 'content_s3_2',
-                       ['sha1', 'path', 'size'], cur)
-        return True, None
-    except Exception as e:
-        return False, e
+    with db.transaction() as cur:
+        db.copy_to(list(load_file(path)), 'content_s3_2',
+                   ['sha1', 'path', 'size'], cur)
 
 
 def store_file_and_print_result(db, path):
@@ -47,58 +39,22 @@ def store_file_and_print_result(db, path):
     This prints ok or ko depending on the result.
 
     """
-    res, e = store_file_to_antelink_db(db, path)
-    if res:
+    try:
+        store_file_to_antelink_db(db, path)
         print('%s ok' % path)
-    else:
+    except Exception as e:
         print('%s ko %s' % (path, e))
 
 
-def store_initial_round(dirpath):
-    """Starting from scratch, this will fetch all *.ls files and injects
-    them in db.
-
-    """
-    db = Db.connect(DB_CONN)
-    paths = glob.glob(dirpath + '*.ls')
-    nb_paths = len(paths)
-    for path in paths:
-        store_file_and_print_result(db, path)
-    print('Number of processed files: %s' % nb_paths)
-
-def store_second_round(path):
-    """The first round finished, there were errors. Adapting the code and
-    running this command will finish appropriately the first round.
-
-    """
-    db = Db.connect(DB_CONN)
-    with open(path, 'r') as f:
-        for line in f:
-            line = line.rstrip('\n')
-            if ' ko ' in line:
-                failed_filepath = line.split(' ko ')[0]
-                store_file_and_print_result(db, failed_filepath)
-
-
 if __name__ == '__main__':
-    dir_or_file_path = sys.argv[1]
+    db_url = "%s" % sys.argv[1]
 
-    if not os.path.exists(dir_or_file_path):
-        print('Path %s does not exist.' % dir_or_file_path)
+    db = Db.connect(db_url)
+    for line in sys.stdin:
+        filepath = line.rstrip()
 
-    # expects path to antelink-object-storage containing 4096 file
-    # with .ls extensions
-    if os.path.isdir(dir_or_file_path):
-        # dirpath = '/home/tony/work/inria/antelink/antelink-object-storage/'
-        dirpath = dir_or_file_path
-        store_initial_round(dirpath)  # piping that result in some
-                                      # file is a good idea to keep
-                                      # the result per file
+        if not os.path.exists(filepath):
+            print('Path %s does not exist.' % filepath)
 
-    # expects output of previous conditional run with 1 absolute
-    # filepath separated by 1 space then 'ok|ko' referencing the
-    # success|failure of such file injection
-    elif os.path.isfile(dir_or_file_path):
-        # filepath = '/home/tony/work/inria/repo/swh-environment/swh-loader-antelink/stats/round-1'
-        filepath = dir_or_file_path
-        store_second_round(filepath)
+        if os.path.isfile(filepath):
+            store_file_and_print_result(db, filepath)
