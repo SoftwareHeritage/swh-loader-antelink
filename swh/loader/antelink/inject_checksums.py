88dd5490212f1648ab641aca5f64708516e42f98
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-02-18 10:47:22 +0100

    Improve checksums loader
    + add docstring

--- swh/loader/antelink/inject_checksums.py
+++ swh/loader/antelink/inject_checksums.py
@@ -4,9 +4,13 @@
 # See top-level LICENSE file for more information
 
 """Module in charge of loading the data retrieved from sesi's backup.
-The module swh.loader.antelink.compute_checksums will output the data
-needed in csv.  This one will parse and load the data in db
-antelink.content_sesi_all.
+This expects to receive filepaths from sys.stdin.
+Those filepath must point to existing file which are of the form:
+sha1,sha1_git,sha256,length,path,corrupted
+
+This is the output format of module swh.loader.antelink.compute_checksums.
+
+The data are stored in table content_sesi_all.
 
 """
 
@@ -16,41 +20,44 @@ import sys
 from swh.loader.antelink.db import Db
 from swh.core import hashutil
 
-DB_CONN = "service='antelink-swh'"
-
 
 def load_file(path):
     """Load file and yield sha1, pathname couple."""
     with open(path, 'r') as f:
         for line in f:
-            data = line.rstrip('\n').split(',')
-            yield {'sha1': hashutil.hex_to_hash(data[1]),
-                   'sha1_git': hashutil.hex_to_hash(data[1]),
-                   'sha256': hashutil.hex_to_hash(data[2]),
-                   'length': data[3],
-                   'path': data[4],
-                   'corrupted': data[5]}
+            data = line.rstrip().split(',')
+            path = data[4]
+            # some line can be empty on sha1, sha1_git, sha256 (when
+            # huge file or pb during hash computation)
+            if data[0]:
+                yield {'sha1': hashutil.hex_to_hash(data[0]),
+                       'sha1_git': hashutil.hex_to_hash(data[1]),
+                       'sha256': hashutil.hex_to_hash(data[2]),
+                       'length': data[3],
+                       'path': path,
+                       'corrupted': data[5]}
+            else:
+                print('%s skipped.' % path)
 
 
-def store_file_content(path):
+def store_file_content(db_url, path):
     """The first round finished, there were errors. Adapting the code and
     running this command will finish appropriately the first round.
 
     """
-    db = Db.connect(DB_CONN)
+    db = Db.connect(db_url)
     with db.transaction() as cur:
-        db.copy_to(list(load_file(path)), 'content_sesi_all',
+        db.copy_to(load_file(path), 'content_sesi_all',
                    ['sha1', 'sha1_git', 'sha256', 'length',
                     'path', 'corrupted'], cur)
 
 if __name__ == '__main__':
-    filepath = sys.argv[1]
+    db_url = "%s" % sys.argv[1]
 
-    if not os.path.exists(filepath):
-        print('Path %s does not exist.' % filepath)
+    for filepath in sys.stdin:
+        if not os.path.exists(filepath):
+            print('Path %s does not exist.' % filepath)
+            continue
 
-    # expects output of previous conditional run with 1 absolute
-    # filepath separated by 1 space then 'ok|ko' referencing the
-    # success|failure of such file injection
-    if os.path.isfile(filepath):
-        store_file_content(filepath)
+        if os.path.isfile(filepath):
+            store_file_content(db_url, filepath)
