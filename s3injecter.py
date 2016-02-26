4f960191116e4aa77030d16f4ae9684f5f0fc268
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-02-26 11:42:59 +0100

    Add an s3 injecter task

---
+++ s3injecter.py
@@ -0,0 +1,75 @@
+# Copyright (C) 2015  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+
+import logging
+import os
+import subprocess
+
+from swh.core import config, hashutil
+from swh.storage import get_storage
+
+from swh.loader.antelink import utils
+
+
+DRY_RUN = False
+
+
+class AntelinkS3Injecter(config.SWHConfig):
+    """A bulk loader for downloading some file from s3.
+
+    """
+    DEFAULT_CONFIG = {
+        'storage_class': ('str', 'remote_storage'),
+        'storage_args': ('list[str]', ['http://localhost:5000/']),
+        's3_folder': ('string', '/srv/storage/space/antelink/s3'),
+    }
+
+    def __init__(self, config):
+        self.config = config
+
+        s3_folder = self.config['s3_folder']
+        if not s3_folder.endswith('/'):
+            self.config['s3_folder'] = s3_folder + '/'
+
+        self.storage = get_storage(config['storage_class'],
+                                   config['storage_args'])
+
+        self.log = logging.getLogger(
+            'swh.antelink.loader.AntelinkS3Injecter')
+
+    def process_paths(self, paths):
+        for localpath in paths:
+            if not os.path.exists(localpath):
+                self.log.error('%s does not exist!' % localpath)
+                continue
+
+            try:
+                data = utils.to_content(localpath, log=self.log)
+
+                # Check for corruption on sha1
+                origin_sha1 = utils.sha1_from_path(localpath)
+                sha1 = hashutil.hash_to_hex(data['sha1'])
+                if origin_sha1 != sha1:
+                    self.log.warn('(%s, %s) corrupted! %s != %s! Skipped' %
+                                  (localpath, origin_sha1, sha1))
+                    continue
+
+                self.log.info('%s -> swh' % sha1)
+                yield data
+            except Exception as e:
+                self.log.error('Problem during computation of %s: %s' %
+                               (localpath, e))
+            finally:
+                if os.path.exists(localpath):
+                    os.remove(localpath)
+
+
+    def process(self, paths):
+        # Then process them and store in swh
+        data = self.process_paths(
+            (self.config['s3_folder'] + p for p in paths))
+        if data:
+            self.storage.content_add(data)
