d9cc0c0fdc204d5859d8a47fc24004238246afba
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-02-26 11:28:01 +0100

    Only download s3 files and store them
    Keep the directory structure as is (the information is already in
    antelink db in content_s3)

--- swh/loader/antelink/s3downloader.py
+++ swh/loader/antelink/s3downloader.py
@@ -8,13 +8,8 @@ import logging
 import os
 import subprocess
 
-from swh.core import config, hashutil
+from swh.core import config
 from swh.storage import get_storage
-
-from swh.loader.antelink import utils
-
-
-DRY_RUN = False
 
 
 def download_s3_file(s3path, path):
@@ -30,7 +25,6 @@ class AntelinkS3Downloader(config.SWHConfig):
     DEFAULT_CONFIG = {
         'storage_class': ('str', 'remote_storage'),
         'storage_args': ('list[str]', ['http://localhost:5000/']),
-        'db_url': ('string', 'service=antelink-swh'),
         'bucket': ('string', 's3://antelink-object-storage'),
         'destination_path': ('string', '/home/storage/antelink/s3/'),
     }
@@ -53,36 +47,15 @@ class AntelinkS3Downloader(config.SWHConfig):
             'swh.antelink.loader.AntelinkS3Downloader')
 
     def process(self, dirpath):
-        localpath = self.config['destination_path'] + os.path.basename(dirpath)
+        localpath = self.config['destination_path'] + dirpath
         s3path = self.config['bucket'] + dirpath
 
-        if DRY_RUN:
-            self.log.warn('%s -> %s downloaded (dry run)!' %
-                          (s3path, localpath))
-            return
-
-        if os.path.exists(localpath):
-            self.log.warn('%s exists!' % localpath)
-        else:
-            download_s3_file(s3path, localpath)
-
         try:
-            self.log.info('%s -> %s' % (s3path, localpath))
-            data = utils.to_content(localpath, log=self.log)
-
-            # Check for corruption on sha1
-            origin_sha1 = utils.sha1_from_path(localpath)
-            sha1 = hashutil.hash_to_hex(data['sha1'])
-            if origin_sha1 != sha1:
-                self.log.warn('(%s, %s) corrupted! %s != %s! Skipped' %
-                              (s3path, localpath, origin_sha1, sha1))
-                return
-
-            self.log.info('%s -> swh' % sha1)
-            self.storage.content_add([data])
+            if os.path.exists(localpath):
+                self.log.warn('%s exists!' % localpath)
+            else:
+                self.log.info('%s -> %s' % (s3path, localpath))
+                download_s3_file(s3path, localpath)
         except Exception as e:
             self.log.error('Problem during retrieval of %s: %s' %
                            (localpath, e))
-        finally:
-            if os.path.exists(localpath):
-                os.remove(localpath)
