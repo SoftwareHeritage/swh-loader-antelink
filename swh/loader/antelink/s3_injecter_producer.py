f164af64b5ba1691511cf27d344e02c0beae7222
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-03-08 16:19:23 +0100

    Add final flag to inject the remaining s3 files

--- swh/loader/antelink/s3_injecter_producer.py
+++ swh/loader/antelink/s3_injecter_producer.py
@@ -12,12 +12,17 @@ from swh.loader.antelink import utils, storage
 @click.option('--db-url', default='service=swh-antelink', help='Db access.')
 @click.option('--block-size', default=1000, help='Default block size to use.')
 @click.option('--limit', default=None, help='Limit data to fetch.')
-def compute_s3_jobs(db_url, block_size, limit):
+@click.option('--final', is_flag=True, help='Limit data to fetch.')
+def compute_s3_jobs(db_url, block_size, limit, final):
     from swh.scheduler.celery_backend.config import app
     from swh.loader.antelink import tasks  # noqa
 
     store = storage.Storage(db_url)
-    files_gen = store.read_content_s3_not_in_sesi_nor_in_swh(limit)
+    if final:
+        files_gen = store.read_content_s3_not_in_sesi_nor_in_swh_final(limit)
+    else:
+        files_gen = store.read_content_s3_not_in_sesi_nor_in_swh(limit)
+
     for paths in utils.grouper(files_gen, block_size, fillvalue=None):
         app.tasks['swh.loader.antelink.tasks.AntelinkS3InjecterTsk'].delay(
             list(p for p in paths if p))
