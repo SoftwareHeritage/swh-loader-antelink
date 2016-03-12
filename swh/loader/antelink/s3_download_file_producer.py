069999e8b86f4e20ee394e9699c8587054423c96
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-03-12 22:42:31 +0100

    Adapt producer to latest store api

--- swh/loader/antelink/s3_download_file_producer.py
+++ swh/loader/antelink/s3_download_file_producer.py
@@ -8,14 +8,12 @@ import click
 from swh.loader.antelink import utils, storage
 
 
-def s3_files_to_download(db_url, final, limit):
+def s3_files_to_download(db_url, huge, final, limit):
     store = storage.Storage(db_url)
-    if final:
-        files_gen = store.read_content_s3_not_in_sesi_nor_in_swh_final(limit)
-    else:
-        files_gen = store.read_content_s3_not_in_sesi_nor_in_swh(limit)
-    for data in files_gen:
-        yield data[0]
+    files_gen = store.read_content_s3_not_in_sesi_nor_in_swh(
+        huge=huge, final=final, limit=limit)
+    for path, _ in files_gen:
+        yield path
 
 
 @click.command()
@@ -23,12 +21,14 @@ def s3_files_to_download(db_url, final, limit):
 @click.option('--block-size', default=1000, help='Default block size to use.')
 @click.option('--limit', default=None, help='Limit data to fetch.')
 @click.option('--final', is_flag=True, help='Add final s3 files.')
-def compute_s3_files(db_url, block_size, limit, final):
+@click.option('--huge', is_flag=True, help='Add huge s3 files.')
+def compute_s3_files(db_url, block_size, limit, final, huge):
     from swh.scheduler.celery_backend.config import app
     from swh.loader.antelink import tasks  # noqa
 
-    for paths in utils.split_data(s3_files_to_download(db_url, final, limit),
-                                  block_size):
+    for paths in utils.split_data(
+            s3_files_to_download(db_url, huge, final, limit),
+            block_size):
         app.tasks['swh.loader.antelink.tasks.AntelinkS3DownloaderTsk'].delay(
             list(paths))
 
