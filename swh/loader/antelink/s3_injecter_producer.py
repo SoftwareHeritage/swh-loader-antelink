d6b6e788fa0803509f77d695fb0b91ba4b9baf61
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-03-16 13:13:46 +0100

    Filter on unknown sha1 to produce messages

--- swh/loader/antelink/s3_injecter_producer.py
+++ swh/loader/antelink/s3_injecter_producer.py
@@ -5,10 +5,33 @@
 
 import click
 
+from swh.core import hashutil
+from swh.storage import get_storage
 from swh.loader.antelink import utils, storage
 
 
 task_name = 'swh.loader.antelink.tasks.AntelinkS3InjecterTsk'
+
+
+def process_paths(paths):
+    """Compute map from sha1 to localpath.
+    """
+    m = {}
+    for localpath, size in paths:
+        sha1 = hashutil.hex_to_hash(utils.sha1_from_path(localpath))
+        m[sha1] = [localpath, size]
+    return m
+
+
+def retrieve_unknown_sha1s(swhstorage, gen_data):
+    # Compute blocks of 1000 sha1s
+    for paths in utils.split_data(gen_data, block_size=1000):
+        data = process_paths(paths)
+        sha1s_tocheck = list(data.keys())
+        if len(sha1s_tocheck) > 0:
+            # let those inexistent sha1s flow
+            for sha1 in swhstorage.content_missing_per_sha1(sha1s_tocheck):
+                yield data[sha1][0], data[sha1][1]
 
 
 @click.command()
@@ -21,13 +44,13 @@ task_name = 'swh.loader.antelink.tasks.AntelinkS3InjecterTsk'
 @click.option('--dry-run', is_flag=True, help='Dry run.')
 @click.option('--final', is_flag=True, help='To deal with remaining s3 files.')
 @click.option('--huge', is_flag=True, help='To deal with big files.')
+@click.option('--storage-class', default='remote_storage')
+@click.option('--storage-args',
+              default='http://uffizi.internal.softwareheritage.org:5002/')
 def compute_s3_jobs(db_url, block_size, block_max_files, limit, dry_run,
-                    final, huge):
+                    final, huge, storage_class, storage_args):
     from swh.scheduler.celery_backend.config import app
     from swh.loader.antelink import tasks  # noqa
-
-    store = storage.Storage(db_url)
-    gen_data = store.read_content_s3_not_in_sesi_nor_in_swh(huge, final, limit)
 
     # right inputs
     if isinstance(block_size, str):
@@ -38,6 +61,13 @@ def compute_s3_jobs(db_url, block_size, block_max_files, limit, dry_run,
         limit = int(limit)
     if dry_run:
         print('** DRY RUN **')
+
+    store = storage.Storage(db_url)
+    swhstorage = get_storage(storage_class, storage_args.split(','))
+
+    gen_data = retrieve_unknown_sha1s(
+        swhstorage,
+        store.read_content_s3_not_in_sesi_nor_in_swh(huge, final, limit))
 
     nb_total_blocks = 0
     for paths, size in utils.split_data_per_size(gen_data, block_size,
