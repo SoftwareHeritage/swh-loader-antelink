8b70d0c303368f7ecdd46188ea266fae7322bd78
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-03-16 13:34:06 +0100

    Filter on unknown sha1 to produce sesi inject messages

--- swh/loader/antelink/sesiproducer.py
+++ swh/loader/antelink/sesiproducer.py
@@ -6,6 +6,8 @@
 import click
 import sys
 
+from swh.core import hashutil
+from swh.storage import get_storage
 from swh.loader.antelink import storage, utils
 
 
@@ -25,6 +27,27 @@ def gen_path_length_from_stdin():
         yield data[0], int(data[1])
 
 
+def process_paths(paths):
+    """Compute map from sha1 to localpath.
+    """
+    m = {}
+    for localpath, size in paths:
+        sha1 = hashutil.hex_to_hash(utils.sha1_from_path(localpath))
+        m[sha1] = [localpath, size]
+    return m
+
+
+def retrieve_unknown_sha1s(swhstorage, gendata):
+    # Compute blocks of 1000 sha1s
+    for paths in utils.split_data(gendata, block_size=1000):
+        data = process_paths(paths)
+        sha1s_tocheck = list(data.keys())
+        if len(sha1s_tocheck) > 0:
+            # let those inexistent sha1s flow
+            for sha1 in swhstorage.content_missing_per_sha1(sha1s_tocheck):
+                yield data[sha1][0], data[sha1][1]
+
+
 @click.command()
 @click.option('--db-url', help="""Optional db access.
                                   If not specified, wait for stdin entries.
@@ -36,7 +59,11 @@ def gen_path_length_from_stdin():
 @click.option('--limit', default=None, help='Limit data to fetch.')
 @click.option('--dry-run', is_flag=True, help='Dry run.')
 @click.option('--huge', is_flag=True, help='Deal with huge files.')
-def send_jobs(db_url, block_size, block_max_files, limit, dry_run, huge):
+@click.option('--storage-class', default='remote_storage')
+@click.option('--storage-args',
+              default='http://uffizi.internal.softwareheritage.org:5002/')
+def send_jobs(db_url, block_size, block_max_files, limit, dry_run, huge,
+              storage_class, storage_args):
     """Send paths for worker to retrieve from sesi machine.
 
     """
@@ -58,6 +85,9 @@ def send_jobs(db_url, block_size, block_max_files, limit, dry_run, huge):
         gen_data = store.read_content_sesi_not_in_swh(huge, limit)
     else:
         gen_data = gen_path_length_from_stdin()
+
+    swhstorage = get_storage(storage_class, storage_args.split(','))
+    gen_data = retrieve_unknown_sha1s(swhstorage, gen_data)
 
     nb_total_blocks = 0
     for paths, size in utils.split_data_per_size(gen_data, block_size,
